238c35a24f6f9fa4b06a7f56544293f2f0c9f7ef
David Lewis <lewidavi@med.umich.edu>
2020-12-30 00:44:26 -0800

    tweak size of menu icon

--- app/javascript/components/navbar/index.js
+++ app/javascript/components/navbar/index.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
+import React from 'react';
+import PropTypes from "prop-types";
+import { AppBar, Toolbar, IconButton, Typography, useScrollTrigger } from '@material-ui/core';
 import MenuIcon from "@material-ui/icons/Menu";
+
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 const Navbar = () => {
  return (
-   <AppBar position="fixed">
-     <Toolbar>
-     <IconButton edge="start" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-       <Typography variant="h5">Canopy</Typography>
-     </Toolbar>
-   </AppBar>
+   <ElevationScroll>
+     <AppBar position="fixed">
+       <Toolbar>
+         <IconButton edge="start" aria-label="menu">
+           <MenuIcon fontSize="large" />
+         </IconButton>
+         <Typography variant="h5">Canopy</Typography>
+       </Toolbar>
+     </AppBar>
+   </ElevationScroll>
  );
-}
+};
 
-export default Navbar
+export default Navbar;
